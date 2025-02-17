b51fa269dbf76fbd79aa52d14cdbb40cbd600ea9
Cmoc10 <colin_oconnor9@uri.edu>
2025-02-16 20:48:52 -0500

    Client site update

---
+++ client_site/src/components/form.vue
@@ -0,0 +1,20 @@
+<template>
+    <form>
+        <label>Name:</label>
+        <input type="text" required>
+        <label>Email:</label>
+        <input type="email">
+        <label>Phone:</label>
+        <input type="tel" required>
+    </form>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+
+</style>
