7bb5d067d609bd054377e90930eafc2882c56053
Cmoc10 <colin_oconnor9@uri.edu>
2025-02-26 12:49:55 -0500

    Lab5

---
+++ labs/lab5-ajax/js/book-jq.js
@@ -0,0 +1,17 @@
+$("#don-quixote-img").on("click", function() {
+    $("img").fadeTo(500, 0.5);
+    $("#don-quixote-img").fadeTo(500, 1);
+    $("#details").load("data/cervantes-data.html").hide().fadeIn(750);
+});
+
+$("#two-cities-img").on("click", function() {
+    $("img").fadeTo(500, 0.5);
+    $("#two-cities-img").fadeTo(500, 1);
+    $("#details").load("data/dickens-data.html").hide().fadeIn(750);
+});
+
+$("#lotr-img").on("click", function() {
+    $("img").fadeTo(500, 0.5);
+    $("#lotr-img").fadeTo(500, 1);
+    $("#details").load("data/tolkein-data.html").hide().fadeIn(750);
+});
